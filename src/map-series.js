f50a6e3bfc17ac7077ace1932c8957c5952a20ba
jaawerth <jesse.wertheim@gmail.com>
2015-12-10 18:41:59 -0500

    Update map-series.js
    Made to work with an array of promises OR values

--- src/map-series.js
+++ src/map-series.js
@@ -1,14 +1,14 @@
 'use strict';
 
 
-function swMapSeries(mapper, promises) {
-  return promises.reduce(function(prev, promise, i, array) {
+function swMapSeries(mapper, promisesOrVals) {
+  return Promise.all(promisesOrVals).then(values => values.reduce(function(prev, promise, i, array) {
     return prev.then(function() {
       return promise.then(function(result) {
         return mapper(result, i, array);
       }); 
     });
-  }, Promise.resolve());
+  }, Promise.resolve()));
 }
 
 module.exports = swMapSeries;
