7cef93f5b43e5f70e6346b322ec1675448d698d5
Jesse Wertheim <jaawerth@users.noreply.github.com>
2016-01-13 18:05:45 -0500

    rewrote map-seriesto use recursive approach

--- src/map-series.js
+++ src/map-series.js
@@ -4,12 +4,30 @@ const curry = require('core.lambda').curry;
 module.exports = curry(2, mapSeries);
 
 function mapSeries(mapper, promisesOrVals) {
-  return Promise.all(promisesOrVals).then(values => values.reduce(function(prev, promise, i, array) {
-    return prev.then(function() {
-      return promise.then(function(result) {
-        return mapper(result, i, array);
-      }); 
-    });
-  }, Promise.resolve()));
+  return Promise.all( _mapSeries([], promisesOrVals[0], promisesOrVals.slice(1), promisesOrVals, mapper ) );
 }
+
+function _mapSeries(agg, head, tail, array, mapper) {
+  Promise.resolve(head).then(function(result) {
+    agg.push(mapper(result, agg.length, array));
+    return agg;
+  }).then(function(agg) {
+    if (!tail.length) {
+      return agg;
+    } else {
+      return _mapSeries(agg, tail[0], tail.slice(1), array, mapper);
+    }
+
+  });
+}
+
+// function mapSeries(mapper, promisesOrVals) {
+//   return Promise.all(promisesOrVals).then(values => values.reduce(function(prev, promise, i, array) {
+//     return prev.then(function() {
+//       return promise.then(function(result) {
+//         return mapper(result, i, array);
+//       }); 
+//     });
+//   }, Promise.resolve()));
+// }
 
