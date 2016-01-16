d9e126fa8ae7d37dadbb737352db5f861528bf84
Jesse Wertheim <jaawerth@users.noreply.github.com>
2016-01-16 14:08:54 -0500

    removed _promise for now

--- src/_promise.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const isCallable = require('is-callable');
-module.exports = function promise({Promise, create, resolve, reject, all, race}) {
-  for (const opt of [Promise, create, resolve, reject, all, race])
-  Promise = Promise || global.Promise || null;
-
-};
