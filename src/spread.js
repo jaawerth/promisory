cf25ab26b0c8b31ab83660b4a770f6147b4c56ab
Jesse Wertheim <jaawerth@users.noreply.github.com>
2016-01-16 14:37:33 -0500

    updated deps, now using babel-plugin-transform-runtime to avoid the need for shims

--- src/spread.js
+++ src/spread.js
@@ -1,14 +1,9 @@
 'use strict';
 
 const curry     = require('core.lambda').curry;
-const isArray   = require('is-array');
-const {toArray} = require('transducers.js');
 
 module.exports = curry(2, spread);
 
 function spread(handler, iterable) {
-  if (isArray(iterable)) {
-    return handler(...iterable);
-  }
-  return handler(...toArray(iterable));
+  return handler(...iterable);
 }
