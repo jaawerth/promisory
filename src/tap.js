8c7835862ab99315d8c9292d8f81f64daac52536
Jesse Wertheim <jaawerth@users.noreply.github.com>
2016-01-16 14:08:29 -0500

    fixed tap

--- src/tap.js
+++ src/tap.js
@@ -1,6 +1,14 @@
 'use strict';
 const curry = require('core.lambda').curry;
 
+module.exports = curry(2, tap);
+
+/**
+ * For side effects. Run a function on a promise's result, returning original resolution value.
+ * @param  {Function} thenFn  Callback for promise's resolution value
+ * @param  {Promise} promise Promise to run on
+ * @return {Promise}         Promise that resolves to original resolution value
+ */
 function tap(thenFn, promise) {
   return promise.then(function(res) {
     thenFn(res);
